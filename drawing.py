92d4768e42f17d6298b35122719fdafc61b6e7c0
Ptolemyy <1602398059@qq.com>
2023-03-18 13:17:09 +0800

    sb

--- drawing.py
+++ drawing.py
@@ -5,21 +5,20 @@ import cv2
 import math
 import json
 import numpy as np
-import time
 from multiprocessing import Pool
-
+import time
 
 parser = argparse.ArgumentParser(description='test')
-parser.add_argument('--fps',default="6000",type=int,help = "None")
 parser.add_argument('--fps_real',default="600",type=int,help = "None")
 parser.add_argument('--start',default="0",type=int,help = "None")
-parser.add_argument('--color',default="yellow",type=str,help = "None")
 
 args = parser.parse_args()
 
 
-frame = args.fps#渲染帧率
+
 real_frame = args.fps_real #输出帧率
+frame = 10 * real_frame
+
 params = []
 w,h = [32768,24000] #原始画布大小 偶数 单位：像素
 n1 = 0.2 #输出压缩率
@@ -31,7 +30,7 @@ img4 = np.zeros((h1, w1,3), dtype=np.uint8)
 img = cv2.imread("frames/canvas.jpg")
 
 n = 10#成像放大率
-cores = 2
+cores = 4
 
 count = args.start
 
@@ -64,15 +63,16 @@ def absolute_point(param):
     return point_
 def scaled_point(param):
 
-    total_x = w1/2
-    total_y = h1/2
+    total_x1 = w1/2
+    total_y1 = h1/2
     point_ = []
-    point_.append([total_x, total_y, 0])
+    point_.append([total_x1, total_y1, 0])
     for i in range(param.__len__()-1,0,-1):
         if i != 0 and (param[i - 1][0] > 0 or param[i - 1][0] < w1) and (param[i - 1][1] > 0 or param[i - 1][1] < h1):
-            total_x -= param[i][0] * n
-            total_y -= param[i][1] * n
-            point_.append([total_x,total_y,math.fabs(param[i][2] * n)])
+            total_x1 -= param[i][0] * n * n1
+            total_y1 -= param[i][1] * n * n1
+            point_.append([total_x1,total_y1,math.fabs(param[i][2] * n * n1)])
+            print(math.fabs(param[i][2] * n * n1))
     return point_
 def scaled_line(i):
     loss_x = points[i-1][points[i-1].__len__()-1][0] * n - w1 / 2
@@ -95,7 +95,7 @@ def main_draw(i):
     for j in range(i+1,1,-1):
         point = points[j-1]
         try:
-            cv2.line(img2, (p_x, p_y), (int(point[point.__len__() - 1][0]), int(point[point.__len__() - 1][1])),(0, 255, 255), thickness=7) if args.color == 'yellow' else cv2.line(img2, (p_x, p_y), (int(point[point.__len__() - 1][0]), int(point[point.__len__() - 1][1])), (0, 0, 255), thickness=7)
+            cv2.line(img2, (p_x, p_y), (int(point[point.__len__() - 1][0]), int(point[point.__len__() - 1][1])),(0, 255, 255), thickness=7)
         except:
             None
         p_x, p_y = [int(point[point.__len__() - 1][0]), int(point[point.__len__() - 1][1])]
@@ -103,8 +103,8 @@ def main_draw(i):
         print("ended")
         cv2.imwrite("frames/canvas.jpg",img2)
     point = points[i-1]
-    point1 = points1[i-1]
-    point2 = points2[i-1]
+    point1 = np.copy(scaled_point(relative_point(transform(), (1 / frame) * i)))
+    point2 = scaled_line(i)
     for j in range(0, point.__len__() - 1):
         cv2.arrowedLine(img2, (int(point[j][0]), int(point[j][1])), (int(point[j + 1][0]), int(point[j + 1][1])),(255, 255, 255), thickness=8) if j <= 2 else cv2.arrowedLine(img2, (int(point[j][0]), int(point[j][1])), (int(point[j + 1][0]), int(point[j + 1][1])), (255, 255, 255),thickness=2)
         try:
@@ -115,11 +115,12 @@ def main_draw(i):
         cv2.arrowedLine(img3,(int(point1[j+1][0]),int(point1[j+1][1])),(int(point1[j][0]),int(point1[j][1])),(255,255,255),thickness = 4)
         try:
             cv2.circle(img3,(int(point1[j][0]),int(point1[j][1])),int(point1[j][2]),(169,169,169),thickness=1)
+            #print(point1[j][2])
         except:
             None
     for j in range(0, point2.__len__()):
         try:
-            cv2.line(img3, (p_x1, p_y1), (int(point2[j][0]), int(point2[j][1])), (0, 255, 255), thickness=10) if args.color == 'yellow' else cv2.line(img3, (int(point2[j][0]), int(point2[j][1])), (p_x1, p_y1), (0, 0, 255), thickness=7)
+            cv2.line(img3, (p_x1, p_y1), (int(point2[j][0]), int(point2[j][1])), (0, 255, 255), thickness=50)
         except:
             None
         p_x1, p_y1 = [int(point2[j][0]), int(point2[j][1])]
@@ -129,12 +130,13 @@ def main_draw(i):
         cv2.imwrite("frames/" + str(int(count1 / (frame / real_frame))) + ".jpg", img2)
     print(i)
 
-points = [absolute_point(relative_point(transform(), (1 / frame) * i)) for i in range(1, frame+1+int(frame/real_frame))]
-points1 = [scaled_point(relative_point(transform(), (1 / frame) * i)) for i in range(1, frame+1+int(frame/real_frame))]
-points2 = [scaled_line(i) for i in range(1, frame+1 + int(frame/real_frame))]
+
+
+points = np.copy([absolute_point(relative_point(transform(), (1 / frame) * i)) for i in range(1, frame + 1 + int(frame / real_frame))])
 if __name__ == '__main__':
     p = Pool(cores)
-    for i in range(1, frame+1+int(frame/real_frame)):
+    for i in range(1, frame + 1 + int(frame/real_frame)):
+    #for i in range(1,10):
         p.apply_async(main_draw, args=(i,))
     p.close()
     p.join()
